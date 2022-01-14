8b0657c6dc7566377e8df539323ddff75d15b778
Alvaro <alvesccas@alum.us.es>
2022-01-14 16:46:10 +0100

    Control Check alvesccas

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,11 +3,20 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer>{
     List<RecoveryRoom> findAll();
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    @Query("SELECT r FROM RecoveryRoomType r")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
     Optional<RecoveryRoom> findById(int id);
     RecoveryRoom save(RecoveryRoom p);
-    //RecoveryRoomType getRecoveryRoomType(String name);
-    //List<RecoveryRoom> findBySizeMoreThan(double size);
+    
+    @Query("SELECT r FROM RecoveryRoomType r WHERE r.name=?1")    
+    RecoveryRoomType getRecoveryRoomType(String name);
+    @Query("Select r FROM RecoveryRoom r WHERE r.size>?1")
+    List<RecoveryRoom> findBySizeMoreThan(double size);
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,23 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoomType {
-    Integer id;
+@Entity
+@Table(name = "recoveryroom_type")
+public class RecoveryRoomType extends BaseEntity{
+	@NotNull
+	@Size(min=5,max=50)
+	@Column(unique=true, name="name")
     String name;
 }
